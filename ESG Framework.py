d1510e787c6b7c06554866ff4a9a81c5010a6833
Jeanne012 <145570054+Jeanne012@users.noreply.github.com>
2025-03-14 10:14:03 +0100

    ESG Framework.py

--- ESG Framework.py
+++ ESG Framework.py
@@ -118,6 +118,7 @@ with st.expander("See Results"):
     ax.set_ylabel("Percentage of Yes Responses")
     ax.set_title("Stacked Bar Chart of ESG Scores")
     ax.legend(["Environmental", "Social", "Governance"], loc='upper right', fontsize=10, frameon=True)
-    ax.tick_params(axis='x', labelsize=8)  # Reduce label size
+    ax.tick_params(axis='x', labelsize=8)
+    ax.tick_params(axis='y', labelsize=8)# Reduce label size
     st.pyplot(fig)
 
